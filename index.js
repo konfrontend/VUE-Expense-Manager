cf30ef5fe59d5b2926a0787f2fa1180b41cfaaf4
Konstantin Dolbilov <dolbilov@galaxy-labs.ru>
2018-10-14 20:08:39 +0300

    fixed index.js

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 const express = require('express');
+const path = require('path');
 const serveStatic = require('serve-static');
 
-require('./models');
+require('./server/models');
 
 const mongoose = require('mongoose');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const posts = require('./server/routes/api/posts');
 const app = express();
+const port = process.env.PORT || 9999;
 
+console.log(__dirname);
 //Middleware
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(path.join(__dirname + '/dist')));
+app.use('/api/posts/', posts);
 app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 
-
-const port = process.env.PORT || 5000;
-
-const posts = require('./routes/api/posts');
-
-app.use('/api/posts/', posts);
 
 mongoose.connect(process.env.MONGODB_URI, {
 	useNewUrlParser: true
